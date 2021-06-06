485cb607cc93d81e90ae02849f9769f2c8732625
Glenn K. Lockwood <glennklockwood@gmail.com>
2021-06-06 13:20:36 -0700

    move locale setting to common

---
+++ roles/common/files/set_locale.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+#
+# raspi-config is licensed under the terms of the MIT license reproduced below.
+# 
+# #####################################################
+# 
+# Copyright (c) 2012 Alex Bradbury <asb@asbradbury.org>
+# 
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation
+# files (the "Software"), to deal in the Software without
+# restriction, including without limitation the rights to use,
+# copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following
+# conditions:
+# 
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
+
+local LOCALE="$1"
+if ! LOCALE_LINE="$(grep "^$LOCALE " /usr/share/i18n/SUPPORTED)"; then
+  return 1
+fi
+local ENCODING="$(echo $LOCALE_LINE | cut -f2 -d " ")"
+echo "$LOCALE $ENCODING" > /etc/locale.gen
+sed -i "s/^\s*LANG=\S*/LANG=$LOCALE/" /etc/default/locale
+dpkg-reconfigure -f noninteractive locales
